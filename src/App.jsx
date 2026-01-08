d98732e269881fc1bd7ebd4113d3613c3b0c230b
Anuj Hooda <hoodaanuj03@gmail.com>
2026-01-08 16:04:01 +0530

    Setup page routing

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,21 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { createBrowserRouter, createRoutesFromElements, Navigate, Route, RouterProvider } from 'react-router';
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
+      const r=createBrowserRouter(
+    createRoutesFromElements(
+        
+    )
+  )
   return (
     <>
-      
+     <RouterProvider router={r}></RouterProvider> 
+  
+    
     </>
   )
 }
