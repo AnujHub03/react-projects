dfe08a3050f7662d0b31beb490d9c611bcbe11d7
Anuj Hooda <hoodaanuj03@gmail.com>
2026-02-05 10:55:43 +0530

    feat(project-13): fetch API data with loading spinner

--- src/Components/Project 13/Pages/Project13.jsx
+++ src/Components/Project 13/Pages/Project13.jsx
@@ -1,8 +1,49 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Circles } from "react-loader-spinner";
+import ProductTile from '../Product-tile/Index';
 
 const Project13 = () => {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      setLoading(true);
+      try {
+        const response = await fetch("https://fakestoreapi.com/products");
+        const data = await response.json();
+        setProducts(data);
+      } catch (error) {
+        console.error("Error fetching products:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
   return (
-    <div>Project13</div>
+  <>
+      <div>
+        {loading ? (
+          <div className="min-h-screen w-full flex justify-center items-center">
+            <Circles
+              height={"120"}
+              width={"120"}
+              color="white"
+              visible={true}
+            />
+          </div>
+        ) : (
+          <div className=" min-h-[80vh]  grid sm:grid-cols-2 md:grid-cols-3 gap-10 lg:grid-cols-4 max-w-6xl mx-auto p-3">
+            {products.map((product) => (
+              <ProductTile key={product.id} product={product} />
+            ))}
+          </div>
+        )}
+      </div>
+    </>
   )
 }
 
---
+++ src/Components/Project 13/Product-tile/Index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ProductTile = ({ product }) => {
+  return (
+    <div>Index</div>
+  )
+}
+
+export default ProductTile
