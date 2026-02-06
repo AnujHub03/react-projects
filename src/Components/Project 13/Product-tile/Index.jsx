efb713b8f291e857f8cbbf139652cbc207b35c8b
Anuj Hooda <hoodaanuj03@gmail.com>
2026-02-06 10:50:12 +0530

    feat(project-13): implement product tile components

--- src/Components/Project 13/Product-tile/Index.jsx
+++ src/Components/Project 13/Product-tile/Index.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
 
 const ProductTile = ({ product }) => {
   return (
-    <div>Index</div>
-  )
-}
+    <div>
+      <div className="border p-4 m-2 rounded-lg shadow-lg hover:shadow-xl hover:shadow-gray-500 transition-shadow hover:-translate-y-2 duration-300 h-full flex flex-col justify-between gap-4 ">
+        <div className="flex flex-col h-full justify-center">
+          <img
+            src={product?.image}
+            alt={product?.title}
+            className="h-44 w-full object-contain mb-4"
+          />
+        </div>
+        <div className="flex flex-col justify-between flex-grow">
+          <h2 className="text-lg font-semibold mb-2">{product?.title}</h2>
+          <p className="text-gray-700 mb-4">${product?.price}</p>
+          <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
+            Add to Cart
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default ProductTile
+export default ProductTile;
