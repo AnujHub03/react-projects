5148ed4dadadec4166d5e468bcac6951231dbf6c
Anuj Hooda <hoodaanuj03@gmail.com>
2026-02-07 10:23:43 +0530

    feat(project-13): configure Redux state management

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import GlobalState from './Components/Project 12/Context/Index.jsx'
+import { Provider } from 'react-redux'
+import { store } from './Components/Project 13/Storage/Index.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <GlobalState>
+      <Provider store={store}>
       <App />
+      </Provider>
     </GlobalState>
   </StrictMode>,
 )
