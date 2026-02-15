536d7a8ec58789cca83a58274b67244616fa206e
Anuj Hooda <hoodaanuj03@gmail.com>
2026-02-15 17:47:22 +0530

    feat: implement easy-project description page

--- src/Components/About/AboutEasy.jsx
+++ src/Components/About/AboutEasy.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
 
 const AboutEasy = () => {
+  const back = () => {
+    window.history.back();
+  };
   return (
-    <div>AboutEasy</div>
-  )
-}
+    <>
+      <div className=" min-h-screen bg-[url('../Img/about1-5.jpg')] bg-cover bg-center border-transparent border-2 ">
+        <div className="m-auto h-auto bg-gray-500 bg-opacity-10  w-10/12 p-5 rounded-2xl mt-12"></div>
+        <div className="flex justify-center mt-10">
+          <button
+            className="bg-gray-500 p-3 text-xl rounded-xl hover:bg-gray-700 hover:text-white"
+            onClick={back}
+          >
+            {"<<"} Back
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default AboutEasy
+export default AboutEasy;
