10f613b7a1943272390500d8458babe18f784781
Anuj Hooda <hoodaanuj03@gmail.com>
2026-02-23 21:43:11 +0530

    feat: adding details

--- src/Components/About/AboutP4.jsx
+++ src/Components/About/AboutP4.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from "react";
 
 const AboutP4 = () => {
   return (
-    <div>AboutP4</div>
-  )
-}
+    <>
+      <div className="border-2 border-transparent p-5 rounded-xl bg-gray-500 bg-opacity-20 mt-10 w-9/12 m-auto">
+        <h1 className="text-6xl p-3 text-white"> Project 4</h1>
+        <p className="text-2xl p-3 mt-4">
+          📌 Project Description – Theme Toggle with Persistence
+        </p>
+        <p className="text-xl p-2">
+          Implemented a light/dark theme switcher that saves the user’s selected
+          theme using local storage and applies it on page reload.
+        </p>{" "}
+        <p className="text-2xl p-4">🔹 Key Features</p>
+        <p className="text-xl p-2">
+          <ul className="list-disc list-inside">
+            <li>
+              One-Click Theme Switch: Toggle between Light and Dark mode
+              instantly.
+            </li>
+            <li>
+              Theme Persistence: Stores the selected theme using local storage.
+            </li>
+            <li>
+              Automatic Theme Load: Applies the saved theme when the page
+              reloads.
+            </li>
+            <li>Smooth UI Transition: Clean and responsive user experience.</li>
+            <li>
+              Dynamic Class Handling: Updates styles by adding/removing CSS
+              classes.
+            </li>
+          </ul>
+        </p>
+      </div>
+    </>
+  );
+};
 
-export default AboutP4
+export default AboutP4;
--- src/Components/Project 4/Project4.jsx
+++ src/Components/Project 4/Project4.jsx
@@ -1,30 +1,51 @@
-import React from 'react'
-import uselocalStorage from './CustomHook';
-import "./Custom.css"
+import React from "react";
+import uselocalStorage from "./CustomHook";
+import "./Custom.css";
+import { Link } from "react-router";
 
 const Project4 = () => {
-    const [theme,setTheme]=uselocalStorage('themes','dark');
-    const handelClick=()=>{
-        const newTheme=theme==='light' ? 'dark' : 'light';
-        setTheme(newTheme);
-        document.documentElement.className=newTheme;
-    }  
-  
-    
+  const [theme, setTheme] = uselocalStorage("themes", "dark");
+  const handelClick = () => {
+    const newTheme = theme === "light" ? "dark" : "light";
+    setTheme(newTheme);
+    document.documentElement.className = newTheme;
+  };
+
   return (
     <>
-     <div className="text-center text-5xl font-bold pt-10 text-black border-2 border-transparent" data-theme={theme} >
-            <h1> Project 4  </h1>
-          </div>
-    <div className="min-h-screen items-center border-2 border-transparent"  data-theme={theme} >
-
-    <div className="h-Uto w-auto m-auto  items-center text-center p-7 font-semibold rounded-xl mt-32">
-        <h1 className="text-5xl mb-10"> Current Theme is  {theme} </h1>
-        <button className="h-16 w-36  rounded-3xl" onClick={handelClick}> Change Theme</button>
-    </div>
-    </div>
+      <div
+        className="text-center text-5xl font-bold pt-10 text-black border-2 border-transparent"
+        data-theme={theme}
+      >
+        <h1> Project 4 </h1>
+      </div>
+      <div
+        className="min-h-screen items-center border-2 border-transparent"
+        data-theme={theme}
+      >
+        <div className="h-Uto w-auto m-auto  items-center text-center p-7 font-semibold rounded-xl mt-32">
+          <h1 className="text-5xl mb-10"> Current Theme is {theme} </h1>
+          <button className="h-16 w-36  rounded-3xl" onClick={handelClick}>
+            {" "}
+            Change Theme
+          </button>
+        </div>
+        <div className="text-center p-5 bg-gray-600 mt-12 w-9/12 m-auto rounded-3xl bg-opacity-70 border-2 border-transparent">
+          <p className="text-white">
+            {" "}
+            If you wana more about project then click here :{" "}
+            <Link
+              className="text-blue-200 text-lg hover:text-blue-800"
+              to={"/aboutP4"}
+            >
+              {" "}
+              More...
+            </Link>
+          </p>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Project4
+export default Project4;
