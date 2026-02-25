66ee15e5a928c87eb0453bc0102ca9dbc4cbf60a
Anuj Hooda <hoodaanuj03@gmail.com>
2026-02-25 08:31:37 +0530

    feat: adding details

--- src/Components/About/AboutP5.jsx
+++ src/Components/About/AboutP5.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from "react";
 
 const AboutP5 = () => {
   return (
-    <div>AboutP5</div>
-  )
-}
+    <>
+      <div className="border-2 border-transparent p-5 rounded-xl bg-gray-500 bg-opacity-20 mt-10 w-9/12 m-auto">
+        <h1 className="text-6xl p-3 text-white"> Project 5</h1>
+        <p className="text-2xl p-3 mt-4">
+          📌 Project Description – Dynamic Tab Navigation System{" "}
+        </p>
+        <p className="text-xl p-2">
+          Designed a tab-based interface that dynamically displays content based
+          on the selected tab without reloading the page.
+        </p>
+        <p className="text-2xl p-4">🔹 Key Features</p>
+        <p className="text-xl p-2">
+          <ul className="list-disc list-inside">
+            <li>
+              Multiple Tabs on a Single Page: Organizes content into separate
+              sections.
+            </li>
+            <li>
+              Dynamic Content Display: Shows only the selected tab’s content.
+            </li>
+            <li>
+              Active Tab Highlighting: Visually indicates the currently selected
+              tab.
+            </li>
+            <li>No Page Reload Required: Content updates using JavaScript.</li>
+            <li>Clean and Responsive Design: Works smoothly across devices</li>
+          </ul>
+        </p>
+      </div>
+    </>
+  );
+};
 
-export default AboutP5
+export default AboutP5;
--- src/Components/Project 5/Project5.jsx
+++ src/Components/Project 5/Project5.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import { Link } from "react-router";
 
 const Project5 = ({ tabsContent, onChange }) => {
   const [currentTabIndex, setCurrentTabIndex] = useState(0);
@@ -11,7 +12,7 @@ const Project5 = ({ tabsContent, onChange }) => {
   return (
     <div className="text-center min-h-screen bg-[url('../../public/Img/flower.jpg')] bg-cover bg-center">
       <div className="text-center text-5xl font-bold pt-10">
-        <h1>Project 7</h1>
+        <h1>Project 5</h1>
       </div>
 
       <div className="text-center border-2 border-black m-10 p-10 bg-slate-700 bg-opacity-20">
@@ -20,7 +21,7 @@ const Project5 = ({ tabsContent, onChange }) => {
             <div key={tabItem.label}>
               <div
                 className={`flex text-xl bg-slate-300 p-4  text-black rounded-xl hover:bg-slate-800 hover:text-white transition-all duration-300 ${
-                  currentTabIndex === index ? 'bg-blue-600 text-white' : ''
+                  currentTabIndex === index ? "bg-blue-600 text-white" : ""
                 }`}
                 onClick={() => handleChange(index)}
               >
@@ -33,6 +34,19 @@ const Project5 = ({ tabsContent, onChange }) => {
         <div className="text-black text-2xl bg-slate-300 p-4 font-bold bg-opacity-70">
           {tabsContent[currentTabIndex]?.content}
         </div>
+      </div>
+      <div className="text-center p-5 bg-gray-600 mt-52 w-9/12 m-auto rounded-3xl bg-opacity-70 border-2 border-transparent">
+        <p className="text-white">
+          {" "}
+          If you wana more about project then click here :{" "}
+          <Link
+            className="text-blue-200 text-lg hover:text-blue-800"
+            to={"/aboutp5"}
+          >
+            {" "}
+            More...
+          </Link>
+        </p>
       </div>
     </div>
   );
