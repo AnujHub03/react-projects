3dbee56eeabe5490ab5292144e24a601f9f8e371
Anuj Hooda <hoodaanuj03@gmail.com>
2026-03-04 21:10:43 +0530

    feat: adding details

--- src/Components/About/AboutP7.jsx
+++ src/Components/About/AboutP7.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
 
 const AboutP7 = () => {
   return (
-    <div>AboutP7</div>
-  )
-}
+    <>
+      <div className="border-2 border-transparent p-5 rounded-xl bg-gray-500 bg-opacity-20 mt-10 w-9/12 m-auto">
+        <h1 className="text-6xl p-3 "> Project 7</h1>
+        <p className="text-2xl p-3 mt-4">
+          📌 Project Description - Modal Popup on Button Click{" "}
+        </p>
+        <p className="text-xl p-2">
+          The Modal Popup on Button Click is an interactive user interface
+          component that displays a dialog box when a user clicks a button. The
+          modal appears as an overlay on the webpage, focusing the user’s
+          attention on specific content such as forms, messages, confirmations,
+          or additional information.
+        </p>
+        <p className="text-2xl p-4">🔹 Key Features</p>
+        <p className="text-xl p-2">
+          <ul className="list-disc list-inside">
+            <li>Reusable and customizable component</li>
+            <li>Smooth and responsive animation</li>
+            <li>DOM selection and manipulation</li>
+            <li>Background overlay effect</li>
+            <li>Event handling</li>
+          </ul>
+        </p>
+      </div>
+    </>
+  );
+};
 
-export default AboutP7
+export default AboutP7;
--- src/Components/Project 7/Project7.jsx
+++ src/Components/Project 7/Project7.jsx
@@ -1,5 +1,6 @@
-import React, { useRef, useState } from 'react'
-import Modal from './Modal';
+import React, { useRef, useState } from "react";
+import Modal from "./Modal";
+import { Link } from "react-router";
 
 const Project7 = () => {
   const [showModal, setShowModal] = useState(false);
@@ -7,34 +8,52 @@ const Project7 = () => {
 
   const handleModalToggle = () => {
     setShowModal(!showModal);
-  }
+  };
 
   return (
     <>
-      <div className="text-center min-h-screen bg-[url('../../public/Img/bass.jpg')] bg-cover bg-center"> 
+      <div className="text-center min-h-screen bg-[url('../../public/Img/bass.jpg')] bg-cover bg-center">
         <div className="text-center text-5xl font-bold pt-10 border-2 border-transparent">
           <h1>Project 7 </h1>
         </div>
-        
+
         <div className="text-center m-auto text-3xl mt-10 bg-slate-600 bg-opacity-40">
-          <button 
-            className='p-6 hover:bg-slate-700 transition-colors duration-300' 
+          <button
+            className="p-6 hover:bg-slate-700 transition-colors duration-300"
             onClick={handleModalToggle}
           >
-             Open Modal popup
+            Open Modal popup
           </button>
-          
+
           {showModal && (
-            <Modal 
+            <Modal
               ref={modalRef}
               onClose={() => setShowModal(false)}
-              body={<div>This is Model Body . This is just a sample text for modal body content just to show how it works.</div>}
+              body={
+                <div>
+                  This is Model Body . This is just a sample text for modal body
+                  content just to show how it works.
+                </div>
+              }
             />
           )}
         </div>
+        <div className="text-center p-5 bg-gray-600 mt-12 w-9/12 m-auto rounded-3xl bg-opacity-70 border-2 border-transparent">
+          <p className="text-white">
+            {" "}
+            If you wana more about project then click here :{" "}
+            <Link
+              className="text-blue-200 text-lg hover:text-blue-800"
+              to={"/aboutp7"}
+            >
+              {" "}
+              More...
+            </Link>
+          </p>
+        </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Project7
+export default Project7;
