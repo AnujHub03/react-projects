aa802251b8d3c429619feab672c7e9e8d9172a69
Anuj Hooda <hoodaanuj03@gmail.com>
2026-03-10 10:22:42 +0530

    feat: adding details

--- src/Components/About/AboutP10.jsx
+++ src/Components/About/AboutP10.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
 
 const AboutP10 = () => {
   return (
-    <div>AboutP10</div>
-  )
-}
+    <>
+      <div className="border-2 border-transparent p-5 rounded-xl bg-gray-500 bg-opacity-20 mt-10 w-9/12 m-auto">
+        <h1 className="text-6xl p-3 "> Project 10</h1>
+        <p className="text-2xl p-3 mt-4">
+          📌 Project Description - Scroll Page with Window Size Tracker
+        </p>
+        <p className="text-xl p-2">
+          This project is a simple React application that demonstrates page
+          scrolling functionality and window size tracking. The application
+          includes two buttons that allow users to smoothly scroll the page from
+          top to bottom and from bottom to top. It also dynamically tracks and
+          displays the current window width and height, updating automatically
+          when the browser window is resized.
+        </p>
+        <p className="text-2xl p-4">🔹 Key Features</p>
+        <p className="text-xl p-2">
+          <ul className="list-disc list-inside">
+            <li>Button to scroll to the top of the page</li>
+            <li>Button to scroll to the bottom of the page</li>
+            <li>Smooth scrolling animation</li>
+            <li>Real-time window size tracking (width & height)</li>
+            <li>Automatic update when the browser window is resized</li>
+          </ul>
+        </p>
+      </div>
+    </>
+  );
+};
 
-export default AboutP10
+export default AboutP10;
--- src/Components/Project 10/Project10.jsx
+++ src/Components/Project 10/Project10.jsx
@@ -1,62 +1,101 @@
-import React, { useRef } from 'react'
-import Test from './Test';
+import React, { useRef } from "react";
+import Test from "./Test";
+import { Link } from "react-router";
 
 const Project10 = () => {
-    const {data,error,loading }=Test('https://dummyjson.com/products',{});
-    const windowSize=Test();
-    const {width,height}=windowSize;
-    const bottomRef=useRef(null);     
-    function handleScrollToTop() {
-        window.scrollTo({ top: 0, behavior: 'smooth' });
-    }
-    function handelScrollToBottom() {
-        bottomRef.current.scrollIntoView({ behavior: 'smooth' });
-    }
+  const { data, error, loading } = Test("https://dummyjson.com/products", {});
+  const windowSize = Test();
+  const { width, height } = windowSize;
+  const bottomRef = useRef(null);
+  function handleScrollToTop() {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }
+  function handelScrollToBottom() {
+    bottomRef.current.scrollIntoView({ behavior: "smooth" });
+  }
   return (
-    <div className=" min-h-screen bg-[url('../../../Img/abstract.jpg')] bg-cover bg-center"> 
-        <div className=" text-white p-3  rounded-b-lg shadow-lg">
-            <h1 className="text-3xl font-bold text-center mt-10">Project 10 </h1>
-        </div>
-        <div className="mt-10 text-center">
-            <h2 className="text-2xl font-semibold text-center mt-10">Scroll to Top and Bottom feature</h2>
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-5" onClick={handelScrollToBottom}>Scroll to Bottom</button>
+    <div className=" min-h-screen bg-[url('../../../Img/abstract.jpg')] bg-cover bg-center">
+      <div className=" text-white p-3  rounded-b-lg shadow-lg">
+        <h1 className="text-3xl font-bold text-center mt-10">Project 10 </h1>
+      </div>
+      <div className="mt-10 text-center">
+        <h2 className="text-2xl font-semibold text-center mt-10">
+          Scroll to Top and Bottom feature
+        </h2>
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-5"
+          onClick={handelScrollToBottom}
+        >
+          Scroll to Bottom
+        </button>
+      </div>
+      <div className="mt-10">
+        <h2 className="text-2xl font-semibold text-center mt-10">
+          Window Size
+        </h2>
+        <p className="text-center text-xl mt-5 text-white">
+          {" "}
+          Width : {width}px{" "}
+        </p>
+        <p className="text-center text-xl mt-2 text-white">
+          {" "}
+          Height : {height}px{" "}
+        </p>
+      </div>
+      <div className="mt-10">
+        <h2 className="text-2xl font-semibold text-center">Use fetch Hook</h2>
 
-        </div>
-        <div className="mt-10">
-            <h2 className="text-2xl font-semibold text-center mt-10">Window Size</h2>
-            <p className="text-center text-xl mt-5 text-white"> Width : {width}px </p>
-            <p className="text-center text-xl mt-2 text-white"> Height : {height}px </p>
-        </div>
-        <div className="mt-10">
-            <h2 className="text-2xl font-semibold text-center">Use fetch Hook</h2>
-            
-            {
-                loading ? (
-                    <p className="text-center mt-5 text-xl">Loading... Please Wait !! </p>
-                ) : null
-            }
-            { error ? (
-                <p className="text-center mt-5 text-xl text-red-500"> Error: {error} </p>
-            ) : null }
-        <ul style={{listStyleType: 'none', padding: 0}}>
-            { 
-                data && data.products && data.products.length 
-                ? data.products.map(item => (
-                    <li key={item.id} className="text-white text-center mt-5 text-md">
-                        { item.title}
-                    </li>
-                ))
-                : null
-            }
+        {loading ? (
+          <p className="text-center mt-5 text-xl">Loading... Please Wait !! </p>
+        ) : null}
+        {error ? (
+          <p className="text-center mt-5 text-xl text-red-500">
+            {" "}
+            Error: {error}{" "}
+          </p>
+        ) : null}
+        <ul style={{ listStyleType: "none", padding: 0 }}>
+          {data && data.products && data.products.length
+            ? data.products.map((item) => (
+                <li
+                  key={item.id}
+                  className="text-white text-center mt-5 text-md"
+                >
+                  {item.title}
+                </li>
+              ))
+            : null}
         </ul>
-            
-            <div ref={bottomRef} className="text-center mt-10 mb-10">
-            <button  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-5" onClick={handleScrollToTop}>Scroll to Top</button>
-            <h3> This is the bottom of page. In order to reach to top click the button above.</h3>
-            </div>
-        </div>
-    </div>
-  )
-}
 
-export default Project10
+        <div ref={bottomRef} className="text-center mt-10 mb-10">
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-5"
+            onClick={handleScrollToTop}
+          >
+            Scroll to Top
+          </button>
+          <h3>
+            {" "}
+            This is the bottom of page. In order to reach to top click the
+            button above.
+          </h3>
+        </div>
+      </div>
+      <div className="text-center p-5 bg-gray-600 mt-12 w-9/12 m-auto rounded-3xl bg-opacity-70 border-2 border-transparent">
+        <p className="text-white">
+          {" "}
+          If you wana more about project then click here :{" "}
+          <Link
+            className="text-blue-200 text-lg hover:text-blue-800"
+            to={"/aboutP10"}
+          >
+            {" "}
+            More...
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Project10;
